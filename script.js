82ae20cb7978f8f4dd96197713731a02299924c4
MEBZMasondo <mondlime8@gmail.com>
2026-02-09 08:25:14 +0200

    Added documents, started project info links

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 // Place holder
-document.getElementById('contact-form').addEventListener('submit', function(e){
+document.getElementById('contact-form').addEventListener('submit', function (e) {
     e.preventDefault();
     alert('Hello.');
     this.reset();
 });
+
+
+// Load header
+function loadHeader() {
+    fetch("pages/header.html")
+        .then(function (response) {
+            return response.text();
+        })
+        .then(function (data) {
+            document.getElementById("header").innerHTML = data;
+        })
+        .catch(function (error) {
+            console.error("Header failed to load", error);
+        });
+}
